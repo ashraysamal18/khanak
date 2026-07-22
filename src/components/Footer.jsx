7213d9d7c2a9c59cf9399ed0399ff06b02e30e65
Ashray Samal <ashrayrajput18@gmail.com>
2026-07-22 19:30:24 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,7 +33,7 @@ export default function Footer({ onNavigateHome, onNavigateAbout }) {
               <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
               <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
             </svg>
-            khanak.studio
+            khanak_22
           </a>
         </div>
 
